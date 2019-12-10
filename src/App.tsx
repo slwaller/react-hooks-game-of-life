8cceb9ae6c893a0773babd03d54411406b47f9d2
Sam Waller <samuellwaller@gmail.com>
2019-12-10 03:01:35 -0500

    grid html added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+
+const numRows = 50
+const numColumns = 50
 
 const App: React.FC = () => {
-  return (
-      <div>Yo</div>
-  );
-}
+    const [grid, setGrid] = useState(() => {
+        const rows = []
+        for( let i = 0; i < numRows; i++){
+            rows.push(Array.from(Array(numColumns), () => 0))
+        }
 
-export default App;
+        return rows
+
+    })
+
+    console.log(grid)
+
+  return (
+      <div style={{display: 'grid', gridTemplateColumns: `repeat(${numColumns}, 20px)`}}>
+            {grid.map((rows, ri) => 
+                rows.map((col, ci) => (
+                    <div
+                        key={`${ri}-${ci}`}
+                        style={{
+                            width: 20, 
+                            height: 20, 
+                            backgroundColor: grid[ri][ci] ? 'red' : undefined,
+                            border: "solid 1px black"
+                        }}
+                    />
+                ))
+            )}
+      </div>
+  );
+};
+
+
+export default App
